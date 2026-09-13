e622fd90a7036518d97d8f36d022942fe0a95d0d
flakesnoy <abelchstr@gmail.com>
2026-09-13 17:28:15 +0200

    feat: Update files

--- Codium/App.xaml.cs
+++ Codium/App.xaml.cs
@@ -7,4 +7,11 @@ namespace Codium;
 /// </summary>
 public partial class App : Application
 {
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+
+		MainWindow = new MainWindow();
+		MainWindow.Show();
+	}
 }
--- Codium/obj/Debug/net10.0-windows/Codium.AssemblyInfo.cs
+++ Codium/obj/Debug/net10.0-windows/Codium.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820673d2704fb4006ea5620acd9697fd89e9a1a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869f4af95fcb2de5bdeff4e63aabb1bd893f7de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
